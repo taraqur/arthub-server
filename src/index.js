05fc5924cbe6559f72f88cba4b27c4d6e40d3032
taraqur <taraqur003@gmail.com>
2026-06-19 19:57:04 +0600

    init: setup express server, routing, and mongoose connection

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
